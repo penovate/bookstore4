afffde9a9b6be46201da20bc0dd4d57a0356b3fa
Kumalai <jameslai0224@gmail.com>
2025-12-23 19:43:30 +0800

    123

--- src/main/java/bookstore/util/DBUtil.java
+++ src/main/java/bookstore/util/DBUtil.java
@@ -6,8 +6,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static HikariDataSource dataSource;
+	private static HikariDataSource dataSource;
 
+<<<<<<< HEAD
     static { 
         try {
             HikariConfig config = new HikariConfig();
@@ -21,24 +22,39 @@ public class DBUtil {
             config.setMaximumPoolSize(10);
             config.setMinimumIdle(5);
             config.setConnectionTimeout(30000); 
+=======
+	static {
+		try {
+			HikariConfig config = new HikariConfig();
+>>>>>>> master
 
-            dataSource = new HikariDataSource(config);
-            
-        } catch (Exception e) {
-            System.err.println("HikariCP Initialization Failed!");
-            e.printStackTrace();
-            throw new RuntimeException("Database initialization error!", e);
-        }
-    }
+			config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-    public static Connection getConnection() throws SQLException {
-        return dataSource.getConnection(); 
-    }
-    
-    public static void closeConnection(Connection connection) {
+			config.setJdbcUrl("jdbc:sqlserver://localhost:1433;databaseName=bookstore;trustServerCertificate=true");
+			config.setUsername("owner");
+			config.setPassword("12345");
+
+			config.setMaximumPoolSize(10);
+			config.setMinimumIdle(5);
+			config.setConnectionTimeout(30000);
+
+			dataSource = new HikariDataSource(config);
+
+		} catch (Exception e) {
+			System.err.println("HikariCP Initialization Failed!");
+			e.printStackTrace();
+			throw new RuntimeException("Database initialization error!", e);
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	public static void closeConnection(Connection connection) {
 		if (connection != null) {
 			try {
-				connection.setAutoCommit(true); 
+				connection.setAutoCommit(true);
 				connection.close();
 			} catch (SQLException e) {
 				e.printStackTrace();

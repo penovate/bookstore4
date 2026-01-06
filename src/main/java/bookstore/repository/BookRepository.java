c87ddf805769f18183f8c228be22b2eb14268409
penovate <okingopen@gmail.com>
2026-01-06 11:58:26 +0800

    no message

---
+++ src/main/java/bookstore/repository/BookRepository.java
@@ -0,0 +1,21 @@
+package bookstore.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import bookstore.bean.BooksBean;
+
+public interface BookRepository extends JpaRepository<BooksBean, Integer> {
+
+	Optional<BooksBean> findByIsbn(String isbn);
+	@Query("SELECT b FROM BooksBean b WHERE b.genreBean.genreId = :genreId")
+	List<BooksBean> findGenreBean_GenreId(Integer genreId);
+
+	//新增加--
+	List<BooksBean> findByOnShelf(Integer onShelf);
+	
+	
+}

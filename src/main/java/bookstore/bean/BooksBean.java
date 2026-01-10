78e93d0462e8a06a8c29dbc9c3ef19e3511033ed
alex122694 <alex122694@gmail.com>
2026-01-10 17:35:03 +0800

    no message

--- src/main/java/bookstore/bean/BooksBean.java
+++ src/main/java/bookstore/bean/BooksBean.java
@@ -30,6 +30,7 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Table(name = "books")
